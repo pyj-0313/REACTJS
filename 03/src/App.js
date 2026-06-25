d1e0c4e14583f37d4016f5fb14e2d6baa257b6c2
pyj-0313 <pyjun0313@gmail.com>
2026-06-25 17:29:43 +0900

    Day 01

---
+++ 03/src/App.js
@@ -0,0 +1,13 @@
+import MyComponent from "./03Component/MyComponent"
+
+function App() {
+  return (
+    <div className="App">
+      <h1>COMPONENT TEST</h1>
+      <hr/>
+      <MyComponent />
+    </div>
+  );
+}
+
+export default App;
