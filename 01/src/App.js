281893bc2101bd5ba63cb5af624142d42473119b
pyj-0313 <pyjun0313@gmail.com>
2026-06-25 16:05:57 +0900

    01 Day

---
+++ 01/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        TITLE - !
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ 02_EX/src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+
+// 문제(직접 풀기): 02_EX
+import EX_02 from "./02JSX/02_EX";
+// 정답 확인: 아래 줄 주석을 풀고 위 줄을 주석 처리하세요.
+// import EX_02 from "./02JSX/02_EX_정답";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>02_EX (JSX 단계별 연습)</h1>
+
+      <EX_02 />
+    </div>
+  );
+}
+
+export default App;
