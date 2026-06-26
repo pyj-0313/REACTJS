f8cf3183dba83ddcd8005ad051c326fa264b35fb
pyj-0313 <pyjun0313@gmail.com>
2026-06-26 09:13:32 +0900

    02 Day

---
+++ 03_EX/src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+
+// 문제(직접 풀기): 03_EX
+import EX_03 from "./03Component/03_EX";
+// 정답 확인: 아래 줄 주석을 풀고 위 줄을 주석 처리하세요.
+// import EX_03 from "./03Component/03_EX_정답";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>03_EX (Component & Hooks 단계별 연습)</h1>
+
+      <EX_03 />
+    </div>
+  );
+}
+
+export default App;
